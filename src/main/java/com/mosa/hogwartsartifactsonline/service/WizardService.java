aa392b08939cc3afd5b778b35b92eeadd8a39448
muhmedsaeed <mohamed.saied.5755@gmail.com>
2025-02-04 00:59:57 +0200

    Artifact assignment done! Close #3

--- src/main/java/com/mosa/hogwartsartifactsonline/service/WizardService.java
+++ src/main/java/com/mosa/hogwartsartifactsonline/service/WizardService.java
@@ -1,7 +1,9 @@
 package com.mosa.hogwartsartifactsonline.service;
 
+import com.mosa.hogwartsartifactsonline.entity.Artifact;
 import com.mosa.hogwartsartifactsonline.entity.Wizard;
 import com.mosa.hogwartsartifactsonline.exception.ObjectNotFoundException;
+import com.mosa.hogwartsartifactsonline.repo.ArtifactRepository;
 import com.mosa.hogwartsartifactsonline.repo.WizardRepository;
 import com.mosa.hogwartsartifactsonline.utils.IdGenerator;
 import jakarta.transaction.Transactional;
@@ -15,12 +17,14 @@ import java.util.List;
 public class WizardService {
 
     private final WizardRepository wizardRepository;
+    private final ArtifactRepository artifactRepository;
     private final IdGenerator idGenerator;
 
     @Autowired
-    public WizardService(WizardRepository wizardRepository, IdGenerator idGenerator) {
+    public WizardService(WizardRepository wizardRepository, IdGenerator idGenerator, ArtifactRepository artifactRepository) {
         this.wizardRepository = wizardRepository;
         this.idGenerator = idGenerator;
+        this.artifactRepository = artifactRepository;
     }
 
 
@@ -65,6 +69,23 @@ public class WizardService {
         this.wizardRepository.deleteById(wizardId);
     }
 
+
+    public void assignArtifact(Integer wizardId, String artifactId) {
+        // first find the artifact
+        Artifact artifact = this.artifactRepository.findById(artifactId).orElseThrow(() -> new ObjectNotFoundException("artifact", artifactId));
+
+        // second find the wizard
+        Wizard wizard = this.wizardRepository.findById(wizardId).orElseThrow(() -> new ObjectNotFoundException("wizard", wizardId));
+
+        // third assign artifact to wizard
+        if (artifact.getOwner() != null) {
+            artifact.getOwner().removeArtifact(artifact);
+        }
+        wizard.addArtifact(artifact);
+
+
+
+    }
 
 
 
